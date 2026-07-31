f51bf13138d42eacb1fb282462002c64d1fadcaf
louvkrishnaupadhyay <louv2k@gmail.com>
2026-07-31 19:28:56 +0530

    update backend

---
+++ server/models/User.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    username:{
+
+        type:String,
+
+        required:true,
+
+        unique:true,
+
+        trim:true
+
+    },
+
+    email:{
+
+        type:String,
+
+        required:true,
+
+        unique:true,
+
+        lowercase:true
+
+    },
+
+    password:{
+
+        type:String,
+
+        required:true
+
+    },
+
+    wins:{
+
+        type:Number,
+
+        default:0
+
+    },
+
+    losses:{
+
+        type:Number,
+
+        default:0
+
+    },
+
+    draws:{
+
+        type:Number,
+
+        default:0
+
+    },
+
+    createdAt:{
+
+        type:Date,
+
+        default:Date.now
+
+    }
+
+});
+
+module.exports = mongoose.model("User",userSchema);
