7f9247f587d63e20e2782104dbacd6230897e591
louvkrishnaupadhyay <louv2k@gmail.com>
2026-08-01 17:27:42 +0530

    Added the api and context folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import AuthProvider from "./context/AuthContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-    <App />
+    <AuthProvider>
+        <App />
+    </AuthProvider>
 );
